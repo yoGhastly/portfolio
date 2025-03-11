8bffbfdef0e0947eefdfd8d6215d86e615ed505d
Diego Espinosa <78836469+yoGhastly@users.noreply.github.com>
2025-03-10 19:30:04 -0500

    Update about-section.tsx

--- app/components/layout/about-section.tsx
+++ app/components/layout/about-section.tsx
@@ -6,36 +6,33 @@ export function About() {
     <Parallax speed={5}>
       <section className="flex flex-col gap-10 w-full md:max-w-sm mx-auto justify-center mt-5 font-sans px-8 md:px-0">
         <p className="text-gray-600">
-          Passionate about frontend web technologies. I love working at the
-          intersection of creativity and user{" "}
-          <span className="bg-purple-100 text-black">friendly interfaces.</span>{" "}
-          When I&apos;m not building or exploring new web experiences, I&apos;m
-          probably training MMA.
+          I specialize in AI, ML development, and frontend engineering, building intelligent systems that enhance decision-making 
+          in{" "}
+          <span className="bg-purple-100 text-black">finance and operations.</span>{" "}
+          When I&apos;m not solving complex data challenges or improving AI assistants, I’m training MMA.
         </p>
 
         <section className="flex flex-col gap-8 mx-auto justify-center max-w-5xl w-full">
           <p className="font-sans text-xl">
-            A blend of UI and Product engineering.
+            Finance-Focused AI & Data Engineer | Frontend & Product Engineering
           </p>
           <p className="text-gray-600">
-            With a background in design, I{" "}
+            Beyond UI, I collaborate with product and data teams to develop AI-powered tools for {" "}
             <span className="bg-lime-100 text-black">
-              work closely with design focused teams{" "}
+              financial analysis, credit risk assessment, and enterprise automation.{" "}
             </span>
-            to build websites and microsites for companies and individuals.
           </p>
 
           <p className="text-gray-600">
             I have experience working and collaborating on{" "}
-            <span className="bg-red-100 text-black">product teams</span> and
+            <span className="bg-red-100 text-black">D&A teams</span> and
             <span className="bg-amber-100 text-black">
               {" "}
               leading engineering{" "}
             </span>
-            efforts. With my experience in UI and product engineering,{" "}
+            efforts. My experience spans developing{" "}
             <span className="text-black">
-              I solve product problems and build appealing usable web
-              experiences.
+              ML models, optimizing retrieval-augmented generation (RAG) systems, and implementing dynamic data visualization solutions
             </span>
           </p>
         </section>
