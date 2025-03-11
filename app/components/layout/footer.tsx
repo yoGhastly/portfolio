f41085bff32d3167cfb3e026f5494256284e9f79
Diego Espinosa <78836469+yoGhastly@users.noreply.github.com>
2025-03-10 19:35:59 -0500

    Update footer.tsx

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Footer() {
   return (
     <footer className="relative bottom-0 flex p-6 mt-20 flex-col md:flex-row justify-center items-center font-sans text-gray-400">
-      <p>&copy; 2024 Diego Espinosa</p>
+      <p>&copy; 2025 Diego Espinosa</p>
     </footer>
   );
 }
